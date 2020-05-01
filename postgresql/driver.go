75889cc5c55605f78dfbd727a0fb73f3d571c187
Neel Choudhary <neel.choudhary3@gmail.com>
2020-05-01 01:54:53 -0500

    db migrations

--- postgresql/driver.go
+++ postgresql/driver.go
@@ -7,7 +7,7 @@ import (
 	"github.com/neelchoudhary/budgetmanagergrpc/config"
 
 	"github.com/neelchoudhary/budgetmanagergrpc/utils"
-
+	"github.com/golang-migrate/migrate/v4"
 	// Required imports
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
@@ -24,7 +24,29 @@ func ConnectDB(dbConfig *config.DBConfig) *sql.DB {
 	err = db.Ping()
 	utils.LogIfFatalAndExit(err)
 
+	MigrateUp(dbConfig)
+
 	fmt.Println("You connected to your database.")
 
 	return db
+}
+
+// MigrateUp migrate up to most recent migration
+// Used for development and continuous integration
+// migrate create -ext sql -dir db/migrations -seq create_users_table
+// migrate -database postgres://postgres:password@localhost:5432/postgres?sslmode=disable -path db/migrations up
+// migrate -database postgres://postgres:password@localhost:5432/postgres?sslmode=disable -path db/migrations down
+func MigrateUp(dbConfig *config.DBConfig) {
+	fmt.Println("Migrating Up...")
+	sourceURL := "file://postgresql/migrations"
+	// postgres://user:password@host:port/dbname?query
+	localDbURL := fmt.Sprintf("postgres://%s:%s@%s:5432/%s?sslmode=disable",
+		dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Dbname)
+	m, err := migrate.New(sourceURL, localDbURL)
+	utils.LogIfFatalAndExit(err)
+
+	if err := m.Up(); err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println("Finished Migrations. Up to Date.")
 }
